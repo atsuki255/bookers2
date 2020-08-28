69f47caca59153dea02aeebc6e98b3f3a5e5d4ab
atsuki225 <atsuki443@gmail.com>
2020-08-29 00:54:03 +0900

    [finish]6-9

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: 'home#index'
+
+   get 'home/about' => 'home#about'
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  
+  resources :users, only: [:show, :index, :edit, :update]
+  resources :books
+
+end
