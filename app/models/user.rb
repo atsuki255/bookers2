3de928ff4f61c132d0468c82e12da603829482e2
atsuki225 <atsuki443@gmail.com>
2020-08-29 19:26:49 +0900

    [update]6-9

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,5 @@ class User < ApplicationRecord
 
    has_many :books, dependent: :destroy
 
-   attachment :image
+   attachment :profile_image
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0347ff9b42df1ffcbbb6484b4a9a1a27492bc864964cf4c8f6ab4b7f9b80fa7a0d45610382c18f00a226a9392937b3ff9460bec9ebaef82207d70a60d60cc683'
