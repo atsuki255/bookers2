688e4fdfbfea54903d736c7a048bf1cd0c92ffb8
atsuki225 <atsuki443@gmail.com>
2020-09-10 11:55:31 +0900

    [finish]応用課題3(いいね、コメント機能)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -34,6 +34,7 @@ class BooksController < ApplicationController
   	@user = current_user
     @book = Book.find(params[:id])
     @book_new = Book.new
+    @book_comment = BookComment.new
   end
 
   def edit
@@ -59,21 +60,22 @@ class BooksController < ApplicationController
     redirect_to "/books"
   end
 
+  def  ensure_current_user
+      @book = Book.find(params[:id])
+     if @book.user_id != current_user.id
+        redirect_to book_path
+     end
+  end
+
   private
 
   def book_params
-  	 params.require(:book).permit(:title, :body)
+  	 params.require(:book).permit(:title, :body, :user_id)
   end
 
   def user_params
   	params.require(:user).permit(:name,:image,:introduction)
   end
 
-  def  ensure_current_user
-      @book = Book.find(params[:id])
-     if @book.user_id != current_user.id
-        redirect_to books_path
-     end
-  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
    has_many :books, dependent: :destroy
+   has_many :book_comments, dependent: :destroy
 
    attachment :profile_image
 end
