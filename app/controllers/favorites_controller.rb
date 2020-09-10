dd8c4c7385eb287262eff56482ca85b538119905
atsuki225 <atsuki443@gmail.com>
2020-09-10 13:18:40 +0900

    [update]応用課題3(いいね、コメント機能)

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,21 @@
 class FavoritesController < ApplicationController
-	def create
-		book = Book.find(params[:book_id])
-		favorite = current_user.favorites.new(book_id: book.id)
-		favorite.save
-		redirect_back(fallback_location: root_path)
-	end
+  before_action :authenticate_user!
 
-	def destroy
-		book = Book.find(params[:book_id])
-		favorite = current_user.favorites.find_by(book_id: book.id)
-		favorite.destroy
-		redirect_back(fallback_location: root_path)
-	end	
+
+  def create
+    @book.favorites.create(user_id: current_user.id)
+  end
+
+  def destroy
+    favorite = current_user.favorites.find_by(book_id: @book.id)
+    favorite.destroy
+  end
+
+  private
+
+
+
+  def favorite_params
+    params.require(:favorite).permit(:user_id, :book_id)
+  end
 end
